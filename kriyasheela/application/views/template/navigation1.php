6158bdbda9ec00bb986929ef3bb08e7ca07287e5
amruta-miraje <amrutam@aaplweb.co.in>
2023-06-06 11:05:32 +0530

    files changes

--- kriyasheela/application/views/template/navigation1.php
+++ kriyasheela/application/views/template/navigation1.php
@@ -2,7 +2,7 @@
 	<nav class="navbar navbar-expand-lg mb-0">
 		<div class="container-fluid">
 			<div class="logo">
-			<a id="top" href="#"><img src="<?= ("http://localhost/balunand/kriyasheela-p2/kriyasheela/images/ca_logo.png") ?>" width="91" height="30"></a>
+			<a id="top" href="#"><img src="<?= ("http://localhost/kriyasheela-p2/kriyasheela/images/ca_logo.png") ?>" width="91" height="30"></a>
 			</div>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
